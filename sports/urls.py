96aca672d897a5fd2c5364bb50e7e29309bb5b09
Dzvapatsva <g.dzvapatsva@uos.ac.uk>
2025-08-27 10:46:59 +0100

    deploying

--- sports/urls.py
+++ sports/urls.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('/shopping', views.shopping, name='shopping'),
+    path('/cart', views.cart, name='cart'),
+path('/contact', views.contact, name='contact')
 ]
--- sports/views.py
+++ sports/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+def shopping(request):
+    return render(request,'shopping.html')
+def cart(request):
+    return render(request,'cart.html')
+def contact(request):
+    return render(request,'contact.html')
